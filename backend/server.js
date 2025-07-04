35e154ff05745efe273c8698e5a63e0538031bb8
DeepakSagar42 <deepaksagar81@MacBookAir.bbrouter>
2025-07-04 22:56:24 +0530

    Add connect-mongo to dependences

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 require("./config/passport");
 const PORT = 3000;
 
-mongoose.connect("process.env.MONGO_URL")
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
